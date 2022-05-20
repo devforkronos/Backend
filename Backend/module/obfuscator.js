62ac6edb554f4d5e80e1a6029f567235fd28aae5
JUB0T <jforeverything2007@gmail.com>
2022-05-20 07:52:16 +0200

    Update

--- Backend/module/obfuscator.js
+++
@@ -1,9 +0,0 @@
-var Luamin = require("lua-format");
-var Settings = {
-    RenameVariables: true,
-    RenameGlobals: false,
-    SolveMath: true,
-};
-module.exports = function main(Code) {
-    return Luamin.Minify(Luamin.Uglify(Code, Settings), Settings);
-};
