372e6017ec9f88ae4d15e49f588fcc7b56fda7ab
JUB0T <jforeverything2007@gmail.com>
2022-05-21 10:16:12 +0200

    update

--- module/obfuscator.js
+++
@@ -1,9 +0,0 @@
-const Luamin = require("lua-format");
-const Settings = {
-    RenameVariables: true,
-    RenameGlobals: false,
-    SolveMath: true,
-};
-module.exports = function main(Code) {
-    return Luamin.Minify(Luamin.Uglify(Code, Settings), Settings);
-};
--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express").Router();
-Router.get("/", async function (req, res) {
-    res.json({
-        Success: true,
-        Message: "Backend Server Working",
-        ErrCode: "200",
-    });
-});
-module.exports = Router;
