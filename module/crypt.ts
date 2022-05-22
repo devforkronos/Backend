54fe426fdbea308ffb86078ddf157d68b6cd0a12
JUB0T <jforeverything2007@gmail.com>
2022-05-22 22:12:04 +0200

    no encryption

--- module/crypt.ts
+++ module/crypt.ts
@@ -28,34 +28,37 @@ module.exports = {
     });
   },
   encrypt: (text) => {
-    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
-    const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
-    const applySaltToChar = (code) =>
-      textToChars(salt).reduce((a, b) => a ^ b, code);
-    try {
-      return text
-        .split("")
-        .map(textToChars)
-        .map(applySaltToChar)
-        .map(byteHex)
-        .join("");
-    } catch {
-      return text;
-    }
+    return text;
+
+    // const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    // const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+    // const applySaltToChar = (code) =>
+    //   textToChars(salt).reduce((a, b) => a ^ b, code);
+    // try {
+    //   return text
+    //     .split("")
+    //     .map(textToChars)
+    //     .map(applySaltToChar)
+    //     .map(byteHex)
+    //     .join("");
+    // } catch {
+    //   return text;
+    // }
   },
   decrypt: (encoded) => {
-    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
-    const applySaltToChar = (code) =>
-      textToChars(salt).reduce((a, b) => a ^ b, code);
-    try {
-      return encoded
-        .match(/.{1,2}/g)
-        .map((hex) => parseInt(hex, 16))
-        .map(applySaltToChar)
-        .map((charCode) => String.fromCharCode(charCode))
-        .join("");
-    } catch {
-      return encoded;
-    }
+    return encoded;
+    // const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    // const applySaltToChar = (code) =>
+    //   textToChars(salt).reduce((a, b) => a ^ b, code);
+    // try {
+    //   return encoded
+    //     .match(/.{1,2}/g)
+    //     .map((hex) => parseInt(hex, 16))
+    //     .map(applySaltToChar)
+    //     .map((charCode) => String.fromCharCode(charCode))
+    //     .join("");
+    // } catch {
+    //   return encoded;
+    // }
   },
 };
